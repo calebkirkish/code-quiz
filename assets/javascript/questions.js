fa46c4ba4a08103f2d2614c1fc504f0f3e5c851f
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-07-07 18:20:10 -0700

    added questions inside questions.js, added some links to the main homepage

---
+++ assets/javascript/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        title: "Who is the villain in 'Austin Powers'?",
+        choices: ["Dr. Evil", "Professor Dementor", "Dastardly Dan"],
+        answer: "Dr. Evil"
+    },
+
+    {
+        title: "In 'Willy Wonka & the Chocolate Factory,' who is the first kid the Oompa Loompas sing out",
+        choices: ["Augustus Gloop", "Mike Teavee", "Violet Beauregarde"],
+        answer: "Augustus Gloop"
+    },
+
+    {
+        title: "In 'Shrek' What does Shrek compare ogres to?",
+        choices: ["Parfaits", "Donkeys", "Onions"],
+        answer: "Onions"
+    },
+
+    {
+        title: "Finish the quote from 'Lord of the Rings: The Fellowship of the Ring': 'One does not simply walk into ________'",
+        choices: ["Sauron", "Mordor", "The Shire"],
+        answer: "Mordor"
+    },
+
+    {
+        title: "What is the name of Hiccup's dragon in 'How to Train Your Dragon'?",
+        choices: ["swift", "Renegade", "Toothless"],
+        answer: "Toothless"
+    },
+
+    {
+        title: "What is the name of the lunar module in 'Apollo 13'?",
+        choices: ["Aquarius", "Odyssey", "Saturn"],
+        answer: "Aquarius"
+    }
+
+];
